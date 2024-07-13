b0334d0118560254600ab5a7d173b9957929db7b
hyein604 <hyein66044.gmail.com>
2024-07-13 16:20:59 +0900

    first commit

---
+++ app/src/main/java/com/hyein/dice_app/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.hyein.dice_app
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import com.hyein.dice_app.databinding.ActivityMainBinding
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val diceImage1 = binding.dice1
+        val diceImage2 = binding.dice2
+
+        binding.diceStartBtn.setOnClickListener{
+            Toast.makeText(this, "주사위 Go!", Toast.LENGTH_LONG).show()
+            val number1 = Random.nextInt(1,6)
+            val number2 = Random.nextInt(1,6)
+
+            when(number1){
+                1 -> diceImage1.setImageResource(R.drawable.dice_1)
+                2 -> diceImage1.setImageResource(R.drawable.dice_2)
+                3 -> diceImage1.setImageResource(R.drawable.dice_3)
+                4 -> diceImage1.setImageResource(R.drawable.dice_4)
+                5 -> diceImage1.setImageResource(R.drawable.dice_5)
+                6 -> diceImage1.setImageResource(R.drawable.dice_6)
+            }
+
+            when(number2){
+                1 -> diceImage2.setImageResource(R.drawable.dice_1)
+                2 -> diceImage2.setImageResource(R.drawable.dice_2)
+                3 -> diceImage2.setImageResource(R.drawable.dice_3)
+                4 -> diceImage2.setImageResource(R.drawable.dice_4)
+                5 -> diceImage2.setImageResource(R.drawable.dice_5)
+                6 -> diceImage2.setImageResource(R.drawable.dice_6)
+            }
+        }
+
+
+
+
+
+
+    }
+}
